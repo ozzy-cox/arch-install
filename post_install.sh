2ac76dc414bf60cbdb413364c368f42b92ee81e0
ozank <ozan.koksal@turboard.com>
2023-01-24 17:25:13 +0300

    moved custom commands to post-install script

--- post_install.sh
+++ post_install.sh
@@ -1,16 +1,23 @@
+
 #!/bin/bash
 sudo pacman -Syu
 git config --global user.email ""
-git clone https://aur.archlinux.org/paru.git
-cd paru
+git clone https://aur.archlinux.org/paru.git /opt/paru
+cd /opt/paru
 makepkg -si
 cd ~
+
+git clone https://github.com/ozzy-cox/dotfiles.git /home/ozank/.dotfiles
+
+#TODO install omz
+#TODO install pyenv
+#TODO install nvm
 
 if [condition]
 then
    paru -S nvidia nvidia-utils lib32-nvidia-utils nvidia-prime
    sudo bash -c "echo -n "ibt=off split_lock_detect=off" >> /boot/loader/entries/*" 
-   # sudo pacman -S xf86-video-
+   # sudo pacman -S xf86-video-mesa
 else
    # statement2
 fi
@@ -20,6 +27,4 @@ fi
 sudo pacman -S xf86-video-vmware xorg-server xorg-xinit xorg-xsetroot base-devel
 mkdir bin
 cd bin
-git clone https://aur.archlinux.org/st
-git clone https://aur.archlinux.org/dwm
 
