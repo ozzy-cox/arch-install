089eff18d85836d2c284e190529417fb0656be0c
ozank <ozan.koksal@turboard.com>
2023-01-24 22:57:34 +0300

    added dotfiles dir

--- post_install.sh
+++ post_install.sh
@@ -1,30 +1,22 @@
 
 #!/bin/bash
 sudo pacman -Syu
-git config --global user.email ""
+git config --global user.email "ozankoksal@hotmail.com"
+git config --global user.name "ozank"
+
+# pull dotfiles
+git clone https://github.com/ozzy-cox/dotfiles.git /home/ozank/.dotfiles
+
+# install paru
 git clone https://aur.archlinux.org/paru.git /opt/paru
 cd /opt/paru
 makepkg -si
 cd ~
 
-git clone https://github.com/ozzy-cox/dotfiles.git /home/ozank/.dotfiles
+# install omz
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-#TODO install omz
-#TODO install pyenv
-#TODO install nvm
+# install pyenv
+curl https://pyenv.run | bash
 
-if [condition]
-then
-   paru -S nvidia nvidia-utils lib32-nvidia-utils nvidia-prime
-   sudo bash -c "echo -n "ibt=off split_lock_detect=off" >> /boot/loader/entries/*" 
-   # sudo pacman -S xf86-video-mesa
-else
-   # statement2
-fi
-
-# Skip this step for vm
-# 
-sudo pacman -S xf86-video-vmware xorg-server xorg-xinit xorg-xsetroot base-devel
-mkdir bin
-cd bin
-
+# install nvm
