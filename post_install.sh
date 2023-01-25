fd54ae9b2b857fa63d0cb94e8d6c1c8709624083
ozank <ozan.koksal@turboard.com>
2023-01-25 17:28:57 +0300

    changed post-install sh paru and omz X

--- post_install.sh
+++ post_install.sh
@@ -1,22 +1,26 @@
 
 #!/bin/bash
 sudo pacman -Syu
-sudo chsh -s /usr/bin/zsh 
+git clone https://github.com/ozzy-cox/arch-install.git
+
+# install omz
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+mv ~/arch-install/.zshrc ~
+exec $SHELL
 
 git config --global user.email "ozankoksal@hotmail.com"
 git config --global user.name "ozank"
 
 # pull dotfiles
-git clone https://github.com/ozzy-cox/dotfiles.git /home/ozank/.dotfiles
+# git clone https://github.com/ozzy-cox/dotfiles.git /home/ozank/.dotfiles
 
 # install paru
 git clone https://aur.archlinux.org/paru.git /opt/paru
-cd /opt/paru
 makepkg -si
-cd ~
 
 # install omz
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 
 # install pyenv
 curl https://pyenv.run | bash
