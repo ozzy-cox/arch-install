c32218fd1df81974446bdaed82346a28f476dd87
ozank <ozan.koksal@turboard.com>
2023-02-23 11:07:02 +0300

    changed order of commands

--- post_install.sh
+++ post_install.sh
@@ -4,14 +4,6 @@ sudo pacman -Syu
 cd ~
 git clone https://github.com/ozzy-cox/dotfiles.git
 
-# install omz
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-ln -s ~/dotfiles/zshrc ~/.zshrc
-exec $SHELL
-
-git config --global user.email "ozankoksal@hotmail.com"
-git config --global user.name "ozank"
-
 # install paru
 git clone https://aur.archlinux.org/paru.git /opt/paru
 makepkg -si
@@ -21,3 +13,13 @@ curl https://pyenv.run | bash
 
 # install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
+# install omz
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+ln -s ~/dotfiles/zshrc ~/.zshrc
+exec $SHELL
+
+git config --global user.email "ozankoksal@hotmail.com"
+git config --global user.name "ozank"
+
+
