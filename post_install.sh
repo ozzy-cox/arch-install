0f8f15133a60fb0e4ed536ab79cc4c161ca3fd11
Ozan Köksal <62054902+ozzy-cox@users.noreply.github.com>
2023-02-23 10:31:29 +0300

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,7 +1,6 @@
 
 #!/bin/bash
 sudo pacman -Syu
-git clone https://github.com/ozzy-cox/arch-install.git
 
 # install omz
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -11,21 +10,12 @@ exec $SHELL
 git config --global user.email "ozankoksal@hotmail.com"
 git config --global user.name "ozank"
 
-# pull dotfiles
-# git clone https://github.com/ozzy-cox/dotfiles.git /home/ozank/.dotfiles
-
 # install paru
 git clone https://aur.archlinux.org/paru.git /opt/paru
 makepkg -si
-
-# install omz
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 
 # install pyenv
 curl https://pyenv.run | bash
 
 # install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-
-# install vscode
